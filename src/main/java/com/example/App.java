b4c3d13b900a3e07f1f9a01e80b6ebb531f45648
prabhatram <prabhat.ram@oulu.fi>
2024-02-16 18:44:10 +0200

    just addition

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -9,5 +9,9 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        int a = 10;
+        int b = 20;
+        int c = a + b;
+        System.out.println("The value of adding " + a + " with " + b + " is " + c);
     }
 }
